6343098e0a25d6995b601d10b69ecfb1ad3dc420
Mohammed Bastawy <mohammedsbastawy@gmail.com>
2025-09-10 12:59:10 +0000

    نفس المشكلة

--- src/components/ui/logo.tsx
+++ src/components/ui/logo.tsx
@@ -1,21 +1,15 @@
 
 import * as React from "react";
 import { cn } from "@/lib/utils";
-import Image from "next/image";
 
 export const Logo: React.FC<{ className?: string }> = ({ className }) => {
     return (
         <div className={cn("relative w-full h-full flex items-center justify-center", className)}>
-            <Image
+            {/* eslint-disable-next-line @next/next/no-img-element */}
+            <img
                 src="/LOGO.PNG"
                 alt="Atlas Logo"
-                fill
-                sizes="100%"
-                style={{ 
-                    objectFit: 'contain',
-                }}
-                className="rounded-md"
-                unoptimized
+                className="w-full h-full object-contain rounded-md"
             />
         </div>
     );
