74dd2a0bfb47878ab60e9790bd92b260a812e3ef
Mohammed Bastawy <mohammedsbastawy@gmail.com>
2025-09-11 06:57:24 +0000

    تمام

--- src/components/ui/logo.tsx
+++ src/components/ui/logo.tsx
@@ -1,14 +1,17 @@
 
 import * as React from "react";
 import { cn } from "@/lib/utils";
-import logoSrc from "../../../LOGO.png";
+
+// As the image is now static and within the public folder, 
+// we can reference it directly.
+const logoSrc = "/LOGO.png";
 
 export const Logo: React.FC<{ className?: string }> = ({ className }) => {
     return (
         <div className={cn("relative w-full h-full flex items-center justify-center", className)}>
             {/* eslint-disable-next-line @next/next/no-img-element */}
             <img
-                src={logoSrc.src}
+                src={logoSrc}
                 alt="Atlas Logo"
                 className="w-full h-full object-contain rounded-md"
             />
