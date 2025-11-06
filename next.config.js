9257ccb97c25963861214cb12201669783c8567a
Mohammed Bastawy <mohammedsbastawy@gmail.com>
2025-11-06 09:42:28 +0000

    PS D:\download\mac> npm run dev
    > nextn@0.1.0 dev
    > next dev -p 9002
    
    D

---
+++ next.config.js
@@ -0,0 +1,31 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+  experimental: {
+    allowedDevOrigins: ["http://localhost:9002", "http://192.168.3.84:9002"],
+  },
+};
+
+module.exports = nextConfig;
